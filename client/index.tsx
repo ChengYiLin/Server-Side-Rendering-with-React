2c5b0fd1ec0bad199b1ba0d4073644a7aedea63d
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-03-17 23:53:38 +0800

    fix: Add Props to client side render

--- client/index.tsx
+++ client/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 
 // 👉 Render
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App serverData={null} />,
+// );
 
+// 👉 Hydration
 const serverData = window?.__SERVER_DATA__ || null;
 serverData && delete window?.__SERVER_DATA__;
 
-// 👉 Hydration
 ReactDOM.hydrateRoot(
     document.getElementById('root'),
     <App serverData={serverData} />,
